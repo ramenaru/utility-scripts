7e22d9c9bce952a2004700fa507fba521b8ccd34
ramen0w0 <ramenarudev@gmail.com>
2024-05-17 17:12:35 +0700

    feat(hotfix | update package): fixing typo

--- installer.sh
+++ installer.sh
@@ -27,7 +27,7 @@ declare -A packages
 packages[Essentials]="xorg xorg-server base-devel htop zsh curl openssh openssl unzip zip wget clang nodejs npm docker firefox chromium
 nginx ffmpeg mpv bluez bluez-utils ttf-liberation ttf-dejavu ttf-font-awesome rsync less reflector fzf go noto-fonts-cjk nerd-fonts alacritty feh upower acpi flameshot"
 packages[WindowManager]="lxappearance nitrogen i3 lightdm lightdm-gtk-greeter dmenu brightnessctl"
-packages[WindowManager]="whois neofetch discord yt-dlp gimp kdenlive virtualbox"
+packages[Misc]="whois neofetch discord yt-dlp gimp kdenlive virtualbox"
 
 install_packages() {
     for key in "${!packages[@]}"
