8cf1489daddd2b7d9fd3db4c44f762b453918a2b
ramenaru <ramenarudev@gmail.com>
2024-05-21 11:04:47 +0700

    feat(add packages): adding picom as compositor for i3 config

--- installer.sh
+++ installer.sh
@@ -26,7 +26,7 @@ confirm() {
 declare -A packages
 packages[Essentials]="xorg xorg-server base-devel htop zsh curl openssh openssl unzip zip wget clang nodejs npm docker firefox chromium
 nginx ffmpeg mpv bluez bluez-utils ttf-liberation ttf-dejavu ttf-font-awesome rsync less reflector fzf go noto-fonts-cjk nerd-fonts alacritty feh upower acpi flameshot"
-packages[WindowManager]="lxappearance nitrogen i3 lightdm lightdm-gtk-greeter dmenu brightnessctl"
+packages[WindowManager]="lxappearance nitrogen i3 lightdm lightdm-gtk-greeter dmenu brightnessctl picom"
 packages[Misc]="whois neofetch discord yt-dlp gimp kdenlive virtualbox"
 
 install_packages() {
