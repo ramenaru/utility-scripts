bac9f9c237e2fbe4228f5bebc20cf81d728e3273
ramen0w0 <ramenarudev@gmail.com>
2024-06-12 19:03:13 +0700

    feat(packages): remove unused packages, adding new packages (e. neovim)

--- installer.sh
+++ installer.sh
@@ -25,7 +25,7 @@ confirm() {
 # Define package groups
 declare -A packages
 packages[Essentials]="xorg xorg-server base-devel htop zsh curl openssh openssl unzip zip wget clang nodejs npm docker firefox chromium
-nginx ffmpeg mpv bluez bluez-utils ttf-liberation ttf-dejavu ttf-font-awesome ttf-jetbrains-mono ttf-firacode rsync less reflector fzf go noto-fonts-cjk nerd-fonts alacritty feh upower acpi flameshot bluetoothctl"
+nginx ffmpeg mpv bluez bluez-utils ttf-liberation ttf-dejavu ttf-font-awesome ttf-jetbrains-mono ttf-fira-code rsync less reflector fzf go noto-fonts-cjk nerd-fonts alacritty feh upower acpi flameshot pcmanfm neovim xf86-video-intel"
 packages[WindowManager]="lxappearance nitrogen i3 lightdm lightdm-gtk-greeter dmenu brightnessctl picom"
 packages[Misc]="whois neofetch discord yt-dlp gimp kdenlive virtualbox"
 
